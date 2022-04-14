1c861296c5861c8e6547c8f364c0ae42e98df444
Rowdog0 <103686595+Rowdog0@users.noreply.github.com>
2022-04-14 18:53:38 -0500

    sup

--- School.py
+++ School.py
@@ -7,6 +7,10 @@ def school():
     print("Hello " + name + " you are " + age + " years old" + " you attend " + school)
     subject = input("What is your favorite school subject? ")
     print("Hello " + name + " you are " + age + " years old" + " you attend " + school + " you love doing " + subject)
+    food = input("What is your favorite school food? ")
+    print("Hello " + name + " you are " + age + " years old" + " you attend " + school + " you love doing " + subject + " your favorite school food is " + food)
+    elective = input("What is your favorite elective? ")
+    print("Hello " + name + " you are " + age + " years old" + " you attend " + school + " you love doing " + subject + " your favorite school food is " + food + " your favorite elective is " + elective)
 
 def main():
     talk = input("Do you want to talk about school or house? ")
