f5e5ecdaf985df0e61b58db148c479f19f9f6a91
Rowdog0 <103686595+Rowdog0@users.noreply.github.com>
2022-04-14 14:49:21 -0500

    Sup bby grl

--- School.py
+++ School.py
@@ -1,1 +1,16 @@
+def school():
+    name = input("What is your Name? ")
+    print("Hello " + name)
+    age = input("How old are you? ")
+    print("Hello " + name + " you are " + age + " years old ")
+    school = input("What school do you go to? ")
+    print("Hello " + name + " you are " + age + " years old" + " you attend " + school)
+    subject = input("What is your favorite school subject? ")
+    print("Hello " + name + " you are " + age + " years old" + " you attend " + school + " you love doing " + subject)
 
+def main():
+    talk = input("Do you want to talk about school or house? ")
+    if talk == "school" or "School":
+        school()
+        
+main()
